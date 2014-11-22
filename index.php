d1e94c5d5258b6b4acf8aa02cef979aaabfe3f24
Marina <imari3669@mail.ru>
2014-11-22 14:51:44 +0300

    задание3

--- index.php
+++ index.php
@@ -1,19 +1,208 @@
-<html>
-<head></head>
-<body>
-<form action="index.php" method="post">
-    <input type="text" name="text3">
-    <input type="submit" value="Отправить">
-</form>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>HTML5 Skeleton</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+
+<body class="row">
+
+<nav>
+
+    <ul class="breadcrumb">
+        <img class="glyphicon-search">
+        <li role="presentation"><a href="#">News</a></li>
+        <li><a href="#">Disney</a></li>
+        <li><a href="#">Cartoons</a></li>
+    </ul>
+</nav>
+
+<aside class="col-md-3">
+    <nav>
+        <ul class="nav list-group">
+            <li class="list-group-item"><img class=""><a href="#" class="text-left">Latest articles</a></li>
+            <li class="list-group-item"><img class=""><a href="#" class="text-left">Popular Comments</a></li>
+            <li class="list-group-item"><img class=""><a href="#" class="text-left">Latest Events</a></li>
+            <li class="list-group-item"><img class=""/><a href="#" class="text-left">HTML 5</a></li>
+            <li class="list-group-item"><img src=""/><a href="#" class="text-left">Android Apps</a></li>
+            <li class="list-group-item"><img src=""/><a href="#" class="text-left">Website Usage</a></li>
+            <li class="list-group-item"><img src=""/><a href="#" class="text-left">Email address</a></li>
+        </ul>
+    </nav>
+
+    <div class="side-info">
+        <img src=>
+        <strong>Animated musical</strong>
+        <p>Thisis Photoshop's<br>version of Lorem</p>
+        <hr>
+    </div>
+
+    <div class="side-info">
+        <img class="img-responsive" src="img/side1.jpg">
+        <strong>Animated musical</strong>
+        <p>This is Photoshop's<br>version of Lorem</p>
+        <hr>
+    </div>
+
+    <div class="side-info">
+        <img class="img-responsive" src="img/side2.jpg">
+        <strong>Animated musical</strong>
+        <p>This is Photoshop's<br>version of Lorem</p>
+        <hr>
+    </div>
+
+    <div class="side-info">
+        <img class="img-responsive" src="img/side3.jpg">
+        <strong>Animated musical</strong>
+        <p>This is Photoshop's<br>version of Lorem</p>
+        <button type="submit" class="btn">Load more</button>
+    </div>
+
+    <form role="form">
+        <strong>Search</strong>
+        <br>
+        <div>
+            <input type="search" class="form-control" placeholder="Search.."/>
+            <button class="glyphicon-search"></button>
+            <br>
+            <select name="categories" class="form-control">
+                <option value="">Select Category</option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+            <button type="submit" class="btn" value="Comment">Comment</button>
+        </div>
+    </form>
+
+    <br>
+    <div>
+        <img src=""/>
+        <br>
+        <strong>Admin login</strong>
+        <br>
+        <input type="password" class="form-control">
+        <br>
+        <button type="submit" class="btn">Submit</button>
+    </div>
+</aside>
+
+
+<section class="col-md-9">
+    <article>
+        <img clsss="image-responsive" src="img/post1.jpg"/>
+        <br>
+        <strong>Animated musical adventure-comedy film</strong>
+        <p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor<br>aliquet. Aenean sollicitudin,
+            lorem quis bibendum auctor, nisi elit consequat ipsum,<br> nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulutate cursus a sit amet<br>
+            mauris. Morbi accumsan ipsum velit.
+        </p>
+        <footer>
+            <span class="foot-comment"><img src="img/comment-sign.jpg"/>12 Comment</span>
+            <span class="foot-tweet"><img src="img/twit.jpg"/>500 Tweets</span>
+            <span class="foot-like"><img src="img/like.jpg"/>30</span>
+        </footer>
+    </article>
+    <section>
+        <div class="comments">
+            <div class="date">
+                <data value="">05/03/2013</data>
+                <span>Reply</span>
+            </div>
+            <br>
+            <img class="img-responsive" src="img/comment1.jpg">
+            <strong>GIMMY</strong>
+            <p>Lorem Ipsum is simply dummy the printing and typesetting world Lorem Ipsum has been</p>
+        </div>
+
+        <div class="reply">
+            <div class="date">
+                <data value="">05/03/2013</data>
+                <span>Reply</span>
+            </div>
+            <br>
+            <img class="img-responsive" src="img/comment2.jpg">
+            <strong>GIMMY</strong>
+
+            <p>Lorem Ipsum is simply dummy the printing and typesetting world Lorem Ipsum has been</p>
+        </div>
+
+        <div class="comments">
+            <div class="date">
+                <data value="">05/03/2013</data>
+                <span>Reply</span>
+            </div>
+            <br>
+            <img class="img-responsive" src="img/reply1.jpg">
+            <strong>GIMMY</strong>
+            <p>Lorem Ipsum is simply dummy the printing and typesetting world Lorem Ipsum has been</p>
+        </div>
+
+        <div class="comments">
+            <div class="date">
+                <data value="">05/03/2013</data>
+                <span>Reply</span>
+            </div>
+            <br>
+            <img class="img-responsive" src="img/reply1.jpg">
+            <strong>GIMMY</strong>
+            <p>Lorem Ipsum is simply dummy the printing and typesetting world Lorem Ipsum has been</p>
+        </div>
+
+        <form role="form" action="index11.php" method="post">
+            <strong>New Comment</strong>
+            <br>
+            <input type="text" class="form-control" placeholder="Your name" id="name-comm" name="name"/>
+            <br>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="email address" id="email-comm" name="email">
+            <br>
+            <textarea type="text" class="form-control" placeholder="Comments" id="text-comm" name="comtext"></textarea>
+            <br>
+            <input type="submit" class="btn" value="Comment" id="btn-comm">
+            </div>
+        </form>
+    </section>
+</section>
+
+<script>
+    $("#btn-comm").on("click",function(){
+        var name=$("#name-comm").val();
+        var email=$("#email-comm").val();
+        var text=$("#text-comm").val();
+        $(".comments").text(name,email,text);
+
+    });
+</script>
+
+
 </body>
 </html>
-<?php
-echo $_REQUEST["text3"];
-?>
-<?php
-echo file_put_contents("text.txt", $_REQUEST["text3"])?'wer':'w3';
-$imploded = implode(",",array(2,5,6));
-echo $imploded;
-$exploded = explode(",",$imploded);
-print_r($exploded);
-var_dump($exploded);
+
+
+<div class = "comments">
+    <div class="date">
+        <data value="">05/03/2013</data>
+        <span>Reply</span>
+    </div>
+    <img class="img-responsive" src="img/reply1.jpg">
+    <?php
+
+
+
+    $a = array($_REQUEST["name"], $_REQUEST["email"], $_REQUEST["comtext"]);
+    for ($a[0]=$name; ; $a++){
+        echo file_put_contents("newcomment.txt", $_REQUEST["$a[0]"]);
+        $imploded= implode(" ", array($a[0]));
+        echo $imploded;
+
+    }
+    ?>
+
+    /*
+    //$exploded = explode(",",$imploded);
+    //print_r($exploded);
+    */
