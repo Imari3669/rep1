bb4b126a55bf23edca4a902fb243fec40db66991
Marina <imari3669@mail.ru>
2014-11-23 14:04:01 +0300

    задание4

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_connect = mysql_connect('localhost', 'root', '123');
+$db_connect = mysql_connect('127.0.0.1', 'root', '1234');
 if($db_connect !==false){
-    mysql_select_db('my_new_database', $db_connect);
-    $query = mysql_query('SELECT * FROM my_new_table', $db_connect);
+    mysql_select_db('database1', $db_connect);
+    $query = mysql_query('SELECT * FROM users', $db_connect);
     if ($query!==false) {
        while ($row = mysql_fetch_assoc($query)) {
            echo $row['id'];
